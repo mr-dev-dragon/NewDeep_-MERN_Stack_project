324c5ba8481821efbe801b2e87b0b47052fd1cb0
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-06-16 10:13:30 -0700

    add docker screapt

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 app.get("/", (req, res) => {
-
+    res.send(" hna jage l asasiya ");
+});
+app.get("/admin", (req, res) => {
+    res.send(" admin ana how ahmed  ");
 });
 app.listen(3000, () => console.log('Server started on port 3000'));
